84d641e814a2647307a2016ff33ce78b6ae290f2
Jeremy <JTDriesler@gmail.com>
2020-07-24 10:18:10 -0700

    Added pages app to have a homepage or sorts. This tutorial is complete and functions as it is supposed to.

--- pollster/pollster/urls.py
+++ pollster/pollster/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 
 ]
